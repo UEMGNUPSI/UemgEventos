332525ba17272a12299d7a625bfa6607f2fad7b5
NUPSI-02 <NUPSI-02@NUPSI-PC-02>
2017-03-08 13:23:27 -0300

    correção da chamada do js para barra responsiva funcionar

--- index.php
+++ index.php
@@ -63,9 +63,9 @@ if(!isset($_SESSION['usuario'])){
 
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
