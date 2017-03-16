8c37f4cbed5fcd13d7144e00ff136dac5dfe994e
fernandosif <fernando.mibr@gmail.com>
2017-03-16 13:26:24 -0300

    eventos php atualizado

--- painel/eventos.php
+++ painel/eventos.php
@@ -7,28 +7,75 @@ if(!isset($_SESSION['usuario']) || $_SESSION['admin'] == false){
 	header("Location: /UemgEventos/index.php");
 }
 
+
 require_once('../bd.class.php');
 
 $objBd = new bd();
 $link = $objBd->conecta_mysql();
 
-if(!isset($_SESSION['usuario'])){
-	$login = 0;
+if(isset($_GET['busca'])){
+	$busca = $_GET['busca'];
 }else{
-	$login = 1;
+	$busca = '';
 }
+
+
+if(isset($_GET['sucesso'])){
+	$sucesso = $_GET['sucesso'];
+}else{
+	$sucesso = 0;
+}
+
+
+$sql = "SELECT titulo, id FROM eventos WHERE titulo LIKE '%".$busca."%' AND admin = 1";
+
+$resultado_id = mysqli_query($link, $sql);
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Categorias</title>
+	<title>Eventos</title>
 
 	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
+<?php 
+  if($sucesso == 1){
+  ?>
+  <div class="alert alert-success" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-lable="Fechar">&times;</button>
+      <strong>Novo Evento cadastrado com sucesso!</strong> 
+    </div>
+  <?php  
+  }
+
+ ?>
+ <?php 
+  if($sucesso == 2){
+  ?>
+  <div class="alert alert-success" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-lable="Fechar">&times;</button>
+      <strong>Evento atualizado com sucesso!</strong> 
+    </div>
+  <?php  
+  }
+
+ ?>
+ <?php 
+  if($sucesso == 3){
+  ?>
+  <div class="alert alert-success" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-lable="Fechar">&times;</button>
+      <strong>Evento excluído com sucesso!</strong> 
+    </div>
+  <?php  
+  }
+
+ ?>
+
 <nav class="navbar navbar-inverse navbar-custom  navbar-fixed-top">
 	<div class="container">
 
@@ -56,11 +103,73 @@ if(!isset($_SESSION['usuario'])){
 	            
 	          </ul>
 		<ul class="nav navbar-nav navbar-right">
-			<?php if($login != 0){?><li><a href="/UemgEventos/sair.php">Sair</a></li> <?php } ?>
+			<li><a href="/UemgEventos/sair.php">Sair</a></li>
 		</ul>
 	</div>
 	</div>
 </nav>
+
+<div class="container">
+	<div class="col-md-12">
+		<h2>Buscar Evento</h2>
+		<div class="col-md-5">
+			<form method="get" action="eventos.php">
+				<input type="text" name="busca" placeholder="Busca" class="form-control" required="true">
+				</div>
+				<div class="col-md-3">
+				<button class="btn btn-success">Buscar</button>
+			</form>
+			<a href="cursos.php" class="btn btn-warning">Limpar Busca</a>
+		</div>
+	</div>
+
+	<div class="col-md-12">
+	<hr>
+		<div class="col-md-10">
+			<h2>Eventos</h2>
+		</div>
+		<div class="col-md-2">
+			<a href="evento.php" class="btn btn-primary" style="margin-top: 20px;">Novo Evento</a>
+		</div>
+
+
+	<div class="col-md-12">
+			<table border="1">
+				<tr>
+					<th class="nome">Título</th>
+					<th class="editar">Editar</th>
+					<th class="excluir">Excluir</th>
+				</tr> 
+
+
+				<?php 
+				if($resultado_id){
+					while($admin = mysqli_fetch_array($resultado_id)){
+						echo '<tr>';
+
+						echo "<td class='titulo'>". $admin['titulo'] . '</td>';
+
+						echo "<td class='editar' align='center'> <a class='btn btn-warning' href='evento.php?id=".$evento['id']."'>Editar</a></td>";
+
+						echo "<td class='excluir' align='center'> <button class='btn btn-danger' onclick='confirmar(".$evento['id'].")'>Excluir</button></td>";
+
+						echo '</tr>';
+					}
+				} ?>
+
+			</table>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+	function confirmar(id) {
+    var apagar = confirm("Confirma a exclusão?");
+    if (apagar){
+        location.href = 'apagar_evento.php?id='+ id;
+    }   
+}
+</script>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
