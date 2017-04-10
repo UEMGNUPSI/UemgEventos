bdd7c5b92e6b1d1bd9a990327c08a5d9c8e11e3b
Danilo <Danilo@Danilo-NOT>
2017-04-10 15:07:01 -0300

    relatorio DOMPDF

--- painel/pdfrelatorios.php
+++ painel/pdfrelatorios.php
@@ -1,65 +1,67 @@
-<?php
-
-ob_start();
-$html = ob_get_clean();
-
-$html = utf8_encode($html); 
-
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "uemg_eventos";
-
-$conn =mysqli_connect($servidor,$usuario,$senha,$dbname);
-if(!$conn){
-  die("falha na conexao" . mysql_connect_error());
-}
-else{
-  //echo '</tr>';
-}
+<?php 
 
 
-$id = "12";
-$result_evento = "select * from eventos where id = '$id' LIMIT 1";
-$resultado_evento = mysqli_query($conn,$result_evento);
-$row_evento = mysqli_fetch_assoc($resultado_evento);
+  $servidor = "localhost";
+  $usuario = "root";
+  $senha = "";
+  $dbname = "uemg_eventos";
+  
+  //Criar a conexÃ£o
+  $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+  $html = '<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">';
+  $html = '<table style="font-family: arial,helvetica,sans-serif;">';  
+  $html .= '<thead>';
+  $html .= '<tr>';
+  $html .= '<th>ID</th>';
+  $html .= '<th>Titulo</th>';
+  $html .= '<th>Data inicio</th>';
+  $html .= '<th>Data fim</th>';
+  $html .= '<th>Organizador</th>';
+  $html .= '<th>valor</th>';
+  $html .= '<th>Pagar Para</th>';
+  $html .= '</tr>';
+  $html .= '</thead>';
+  $html .= '<tbody style="text-align: center; background: solid red; font-size: 30px;">';
+  
+  $result_evento = "SELECT * FROM eventos";
+  $resultado_evento = mysqli_query($conn, $result_evento);
+  while($row_evento = mysqli_fetch_assoc($resultado_evento)){
+    $html .= '<tr><td>'.$row_evento['id'] . "</td>";
+    $html .= '<td>'.$row_evento['titulo'] . "</td>";
+    $html .= '<td>'.$row_evento['data_inicio'] . "</td>";
+    $html .= '<td>'.$row_evento['data_fim'] . "</td>";
+    $html .= '<td>'.$row_evento['organizador'] . "</td></tr>";
+    $html .= '<td>'.$row_evento['valor'] . "</td></tr>";
+    $html .= '<td>'.$row_evento['pagar_para'] . "</td></tr>";  
+  }
+  
+  $html .= '</tbody>';
+  $html .= '</table';
 
+  
+  //referenciar o DomPDF com namespace
+  use Dompdf\Dompdf;
 
+  // include autoloader
+  require_once("../dompdf/autoload.inc.php");
 
-$html .= 
-"
-    <html>
-        <body>
-        <h1>Eventos abertos</h1>
-        id: ".row_evento['id']."<br>
-        Título: ".row_evento['titulo']."<br>
-        Descrição: ".row_evento['descricao']."<br>
-        Data de início: ".row_evento['data_inicio']."<br>
-        Data do fim: ".row_evento['data_fim']."<br>
-        Organizador: ".row_evento['organizador']."<br>
-        Valor: ".row_evento['valor']."<br>
-        Pagar para: ".row_evento['paga_para']."<br>
+  //Criando a Instancia
+  $dompdf = new DOMPDF();
+  
+  // Carrega seu HTML
+  $dompdf->load_html('
+      <h1 style="text-align: center; color =red;">Relatório Eventos</h1>
+      '. $html .'
+    ');
 
+  //Renderizar o html
+  $dompdf->render();
 
-        </body>
-    </html>
-";
-
-
-$arquivo = "Cadastro01.pdf";
-
-include("../mpdf60/mpdf.php");
-
-$mpdf = new mPDF();
-
-$mpdf->allow_charset_conversion = true;
-
-$mpdf->charset_in = 'UTF-8';
-
-$mpdf->WriteHTML($html);
-
-$mpdf->Output('meu-pdf','I');
-
-exit();
-
+  //Exibibir a pÃ¡gina
+  $dompdf->stream(
+    "relatorio.pdf", 
+    array(
+      "Attachment" => false //Para realizar o download somente alterar para true
+    )
+  );
 ?>
